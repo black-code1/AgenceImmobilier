b48ab1febdd46ee3041c1e80db1d8d9f1778526c
black-code1 <legrandmahamat1@gmail.com>
2023-04-22 20:36:29 +0100

    Découverte de Laravel 10 : TP : Agence immo, listing

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('base')
+
+@section('content')
+    <div class="bg-light p-5 mb-5 text-center">
+        <div class="container">
+            <h1>Agence lorem ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet dignissimos exercitationem facilis itaque minima perspiciatis quidem sunt? Iste, labore?</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2>Nos derniers biens</h2>
+        <div class="row">
+            @foreach($properties as $property)
+                <div class="col">
+                    @include('property.card')
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
