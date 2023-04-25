a3be0f2e0950104ed4b0f680f4db54d9f4b7a06b
black-code1 <legrandmahamat1@gmail.com>
2023-04-25 08:55:01 +0100

    Découverte de Laravel 10 : Eloquent : Accesseurs et mutateurs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Property;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $properties = Property::with('pictures')->orderBy('created_at', 'desc')->limit(4)->get();
+        $properties = Property::with('pictures')->available()->recent()->orderBy('created_at', 'desc')->limit(4)->get();
+        /** @var User $user */
+        $user = User::first();
+        $user->password = '0000';
+        $user->syncChanges();
+        dd($user->password, $user);
         return view('home', ['properties' => $properties]);
     }
 }
